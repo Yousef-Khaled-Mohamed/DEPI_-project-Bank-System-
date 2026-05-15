a3a2f4baba054a6d8f2b55f431a39ca2439aa358
Amira Hassan <amirahassansobhi@gmail.com>
2026-05-15 10:37:11 +0300

    add PayLoanRequest, TellerDto, UserDto

---
+++ BankManagement.Application/DTO/PayLoanRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement.Application.DTO
+{
+    public record PayLoanRequest(int LoanId, decimal Amount);
+}
